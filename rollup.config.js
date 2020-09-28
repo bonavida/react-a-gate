e9695897ae9f5031e0bdc925be2f8a7a9ef8bddd
Diego Bonavida <gecko.bona@gmail.com>
2020-09-29 00:21:49 +0200

    Added setup config for Jest and React Testing Library

--- rollup.config.js
+++ rollup.config.js
@@ -35,7 +35,7 @@ export default {
   ],
   plugins: [
     sass(sassOptions),
-    typescript(),
+    typescript({ useTsconfigDeclarationDir: true }),
     includePaths(includePathOptions),
     terser(),
   ],
