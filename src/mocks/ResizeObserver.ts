5ef01d4ea443aac191cf3feb66bf17d71eeacb66
Diego Bonavida <gecko.bona@gmail.com>
2020-09-30 01:02:02 +0200

    Empty gh-pages branch

--- src/mocks/ResizeObserver.ts
+++
@@ -1,9 +0,0 @@
-class ResizeObserver {
-  observe = (): void => undefined;
-
-  unobserve = (): void => undefined;
-
-  disconnect = (): void => undefined;
-}
-
-export default ResizeObserver;
--- src/types/types.ts
+++
@@ -1,3 +0,0 @@
-export type Place = 'top' | 'bottom' | 'left' | 'right';
-export type Mode = 'click' | 'hover';
-export type Theme = 'light' | 'dark';
