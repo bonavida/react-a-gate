32aa60dd97ac54e9e8b4e0053c5cbcce94212c0f
bonavida <gecko.bona@gmail.com>
2020-10-02 14:52:38 +0000

    Deploying to gh-pages from  @ 0aba39f1b78b75f1656bce67040757e06f4ab23d 🚀

---
+++ precache-manifest.4ff12bc0038a66829fab3dc372835d97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "965f572a3084e46d545d253eaea88e3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ada9b6c18b1747c1fd5",
+    "url": "/static/css/main.da42e3e8.chunk.css"
+  },
+  {
+    "revision": "e163d62156da91aa3ab4",
+    "url": "/static/js/2.b829d14b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b829d14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ada9b6c18b1747c1fd5",
+    "url": "/static/js/main.b31f94fc.chunk.js"
+  },
+  {
+    "revision": "c4858e452eb293f8b527",
+    "url": "/static/js/runtime-main.037c1b2d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
