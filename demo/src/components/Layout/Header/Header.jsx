233ec059e1af408dcb64d80ce6818b6c76721fcf
Diego Bonavida <gecko.bona@gmail.com>
2021-04-18 00:32:49 +0200

    Add popover page to demo

--- demo/src/components/Layout/Header/Header.jsx
+++ demo/src/components/Layout/Header/Header.jsx
@@ -6,7 +6,7 @@ import './Header.scss';
 const Header = () => (
   <div className="header__container">
     <h1 className="header__title">
-      <Link to="/">react-a-gate</Link>
+      <Link to="/">react-a-gate <span>demos</span></Link>
     </h1>
     <ul className="header__menu">
       <li className="header__menu-item">
--- demo/src/snippets/index.js
+++ demo/src/snippets/index.js
@@ -1,5 +1,6 @@
 import { modal_example_1, modal_example_2_1, modal_example_2_2 } from './modal';
 import { tooltip_example_1, tooltip_example_2 } from './tooltip';
+import { popover_example_1, popover_example_2 } from './popover';
 
 export {
   modal_example_1,
@@ -7,4 +8,6 @@ export {
   modal_example_2_2,
   tooltip_example_1,
   tooltip_example_2,
+  popover_example_1,
+  popover_example_2,
 };
